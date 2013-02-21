f76b349985fdda2046fd9ba5233596d78577bf8c
Aguayo <aguayo@my-lap.(none)>
2013-02-21 01:33:52 -0600

    Agrego la carpeta de Tablas ejercicio en clase

---
+++ PHP/Tablas/TablasMultiplicar.php
@@ -0,0 +1,19 @@
+<?php
+	/*class TablasMultiplicar{
+		public $inicio;
+		public $fin;
+		
+		function _construct($i, $f){
+			$this->inicio = $i;
+			$this->fin = $f;
+		}
+		
+		function ImprimeTabla(){
+			for($tablas = 1; $tabla <=12 ; $tablas++){
+				for($mul = $this->inicio; $sum<=)
+				echo ;
+			}
+		}
+	}*/
+	echo 'hola mundo';
+?>
